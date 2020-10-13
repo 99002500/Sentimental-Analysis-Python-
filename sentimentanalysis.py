f780716f8f8116ef18b9551eeee1a34f6aa3ced7
99002500 <sampreeth.rayadurga@ltts.com>
2020-10-13 15:17:47 +0530

    alignment and corrected snake case

--- sentimentanalysis.py
+++ sentimentanalysis.py
@@ -3,7 +3,6 @@ import tweepy
 from textblob import TextBlob
 from wordcloud import WordCloud
 import pandas as pd
-import numpy as np
 import matplotlib.pyplot as plt
 CONSUMERKEY = "5Wittq9oSwEecQrKo67RoGnZv"
 CONSUMERSECRET = "cS6eAjrdFhRCLTjitROrLeNni2QHZRYjjsNutauaxxLywf33a0"
@@ -12,7 +11,7 @@ ACCESSTOKENSECRET = "ZfuCGRWvkVdkv6Vl4pngMf8u4zyByZuZwoQgOxOHt55M2"
 authenticate = tweepy.OAuthHandler(CONSUMERKEY,CONSUMERSECRET)
 authenticate.set_access_token(ACCESSTOKEN, ACCESSTOKENSECRET)
 api = tweepy.API(authenticate, wait_on_rate_limit=True)
-posts = api.user_timeline(screen_name="@Bill Gates", count=100, lang="en", tweet_mode="extended")
+posts = api.user_timeline(screen_name="@BillGates", count=100, lang="en", tweet_mode="extended")
 print("show the five and recent tweets: \n")
 i=1
 for tweet in posts[0:5]:
@@ -21,34 +20,34 @@ for tweet in posts[0:5]:
 # Stage 0- Importing Libraries and then setting keys for APIs
 df=pd.DataFrame([tweet.full_text for tweet in posts],columns=['Tweets'])
 df.head()
-def clean_Txt(text):
+def clean_txt(text):
     text=re.sub(r'@[A-Za-z0-9]+','',text)
     text=re.sub(r'#','',text)
     text=re.sub(r'RT[\s]+','',text)
     text=re.sub(r'https?:\/\/S+','',text)
     return text
-df['Tweets']=df['Tweets'].apply(clean_Txt)
+df['Tweets']=df['Tweets'].apply(clean_txt)
 print(df)
-def get_Subjectivity(text):
+def get_subjectivity(text):
     return TextBlob(text).sentiment.subjectivity
-def get_Polarity(text):
+def get_polarity(text):
     return TextBlob(text).sentiment.polarity
-df['Subjectivity']=df['Tweets'].apply(get_Subjectivity)
-df['Polarity']=df['Tweets'].apply(get_Polarity)
+df['Subjectivity']=df['Tweets'].apply(get_subjectivity)
+df['Polarity']=df['Tweets'].apply(get_polarity)
 print(df)
 allWords=' '.join([twts for twts in df['Tweets']])
 wordCloud=WordCloud(width=500,height=300,random_state=21,max_font_size=119).generate(allWords)
 plt.imshow(wordCloud,interpolation='bilinear')
 plt.axis('off')
 plt.show()
-def get_Analysis(score):
+def get_analysis(score):
     if score<0:
         return 'Negative'
-    elif score==0:
+    if score==0:
         return 'Neutral'
-    else:
+    if score>0:
         return 'Positive'
-df['Analysis']=df['Polarity'].apply(get_Analysis)
+df['Analysis']=df['Polarity'].apply(get_analysis)
 print(df)
 #Stage 1: Cleaning data (Removing @,#,hyperlinks etc )
 #print positive tweets
@@ -63,18 +62,18 @@ for i in range(0,sortedDF.shape[0]):
 j=1
 sortedDF=df.sort_values(by=['Polarity'],ascending='False')
 for i in range(0,sortedDF.shape[0]):
-    if(sortedDF['Analysis'][i]=='Negative'):
+    if sortedDF['Analysis'][i]=='Negative':
         print(str(j) + ')'+sortedDF['Tweets'][i])
         print()
         j=j+1
 #plot polarity and subjectivity
-plt.figure(figsize=(8,6))  
+plt.figure(figsize=(8,6))
 for i in range(0,df.shape[0]):
-    plt.scatter(df['Polarity'][i],df['Subjectivity'][i],color='green')      
-plt.title('Sentiment Analysis')  
+    plt.scatter(df['Polarity'][i],df['Subjectivity'][i],color='green')
+plt.title('Sentiment Analysis')
 plt.xlabel('Polarity')
 plt.ylabel('Subjectivity')
-plt.show()   
+plt.show()
  #get the percentage of positive tweets
 ptweets=df[df.Analysis=='Positive']
 ptweets=ptweets['Tweets']
@@ -83,7 +82,7 @@ print(round((ptweets.shape[0]/df.shape[0])*100,1))
 ntweets=df[df.Analysis=='Negative']
 ntweets=ntweets['Tweets']
 print(round((ntweets.shape[0]/df.shape[0])*100,1))
-#show the value counts iii8uyh
+#show the value counts
 print(df['Analysis'].value_counts)
 #plot and visualize counts
 plt.title('Sentiment Analysis')
